621f1f4b09db1eea5ad6965cd40d54be1c421334
Unknown <luisa.parra@iesalandalus.org>
2017-12-19 08:58:06 +0100

    Ejemplo ficheros internos

---
+++ EjemploFicherosInternos/src/com/example/ejemploficherosinternos/MainActivity.java
@@ -0,0 +1,116 @@
+package com.example.ejemploficherosinternos;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Context;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
+		Button botonEscribir = (Button) findViewById(R.id.button1);
+		Button botonLeer = (Button) findViewById(R.id.button2);
+		Button botonLeerRaw = (Button) findViewById(R.id.button3);
+		
+		// Si pulsamos en el botón escribir.
+        botonEscribir.setOnClickListener(new OnClickListener() {
+        	public void onClick(View arg0) {
+        		escribirFichero();		
+        	}
+        	});
+       
+        // Si pulsamos en el botón leer.
+        botonLeer.setOnClickListener(new OnClickListener() {
+        	public void onClick(View arg0) {
+        		leerFichero();
+        	}
+        	});    
+        
+		// Si pulsamos en el botón leer Raw.
+        botonLeerRaw.setOnClickListener(new OnClickListener() {
+        	public void onClick(View arg0) {
+        		leerFicheroRaw();
+        	}
+        	});
+	}
+
+	private void escribirFichero(){
+		try
+		{
+			EditText cuadroTxt = (EditText)findViewById(R.id.editText1);
+			String cadena = cuadroTxt.getText().toString();
+			
+			OutputStreamWriter fout= new OutputStreamWriter(
+							openFileOutput("fichero.txt", Context.MODE_PRIVATE));
+ 
+			fout.write(cadena);
+			fout.close();
+		}
+		catch (Exception ex)
+		{
+			Log.e("Ficheros", "Error al escribir fichero en la memoria interna");
+		}
+	}
+
+	private void leerFichero(){
+		try
+		{
+			String linea="", 
+				   cadena="";
+			BufferedReader fin = new BufferedReader(
+		            			new InputStreamReader(openFileInput("fichero.txt")));
+		 
+		    while((linea = fin.readLine())!=null){
+		    	cadena += linea + "\n";
+		    }
+		    fin.close();
+		    
+			EditText cuadroTxt = (EditText)findViewById(R.id.editText1);
+			cuadroTxt.setText(cadena);
+		}
+		catch (Exception ex)
+		{
+		    Log.e("Ficheros", "Error al leer fichero de la memoria interna");
+		}
+	}
+	
+	private void leerFicheroRaw(){
+		try
+		{
+			String linea="", 
+				   cadena="";
+		    InputStream fraw =
+		        getResources().openRawResource(R.raw.fich_raw);
+		 
+		    BufferedReader brin =
+		        new BufferedReader(new InputStreamReader(fraw));
+		 
+		    while((linea = brin.readLine())!=null){
+		    	cadena += linea + "\n";
+		    }
+		    fraw.close();
+		    
+			EditText cuadroTxt = (EditText)findViewById(R.id.editText1);
+			cuadroTxt.setText(cadena);
+		}
+		catch (Exception ex)
+		{
+		    Log.e("Ficheros", "Error al leer fichero del recurso raw");
+		}
+		
+	}
+
+}
